1ad9d17654c888cfe4753f02ac61ea73258b6380
marika <ms.czerwonka@gmail.com>
2022-06-27 20:34:03 +0200

    created simple react todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "First Meeting at School",
+      day: "5-06-2022",
+    },
+    {
+      id: 2,
+      text: "Second Meeting at School",
+      day: "10-06-2022",
+    },
+    {
+      id: 3,
+      text: "Third Meeting at School",
+      day: "15-06-2022",
+    },
+  ]);
+  const [title, setTitle] = useState(`You have ${tasks.length} to do.`);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    setTitle(`You have ${tasks.length - 1} tasks`);
+  };
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+    setTitle(`You have ${tasks.length + 1} tasks`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} title={title} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
+      {showAddTask && <AddTask onAdd={addTask} />}
     </div>
   );
 }
